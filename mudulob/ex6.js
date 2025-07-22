39c27c1312dabe1e5263299658186289b7bb9cb7
IslanMaximo <islanmvieira@gmail.com>
2025-07-22 15:44:56 -0300

    asd

--- mudulob/ex6.js
+++ mudulob/ex6.js
@@ -1,9 +1,9 @@
 var hora = 13
-console.log = `Agora são exatamente ${hora} Horas. `
+console.log = (`Agora são exatamente ${hora} Horas. `)
 if(hora < 12){
-    console.log = "bom dia"
+    console.log = ("bom dia")
 }else if(hora <=18){
-    console.log = "boa tarde"
+    console.log = ("boa tarde")
 }else if(hora < 5){
-    console.log = "vá dormir"
+    console.log = ("vá dormir")
 }
