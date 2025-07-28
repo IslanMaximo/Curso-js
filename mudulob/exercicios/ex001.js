d8d40f7fbba6cedc9f32139f4a609ed49da212e7
IslanMaximo <islanmvieira@gmail.com>
2025-07-28 13:41:48 -0300

    teste

---
+++ mudulob/exercicios/ex001.js
@@ -0,0 +1,8 @@
+var msg = document.getElementById('msg')
+var foto = document.getElementById('foto')
+
+var hora = 12
+
+if( hora >= 5 && hora <= 12){
+    foto.style.background ='url(img/ex001/dia.jpg) center center cover'
+}
